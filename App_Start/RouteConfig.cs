cf043e36673ed3871785101f76200496df2be3ef
williamsoftwarecode <williamsoftwarecode@gmail.com>
2018-06-23 22:07:09 +1000

    Added Customers (model, controller, and view) and changed navbar title to Vidly
    Added Default Customers to Route Config.cs to automatically route to
    /Customers/Index
    
    Added CustomersController.cs in Controllers
    Added Index.cshtml in View
    Added Customers.cs in Models
    
    Changed "Application Name" to "Vidly" in _Layout.cshtml

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace Vidly
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Default Customers",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Customers", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Customers can be found here.";
+
+            return View();
+        }
+    }
+}
